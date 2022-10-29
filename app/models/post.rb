5c25f1b12424da33c9981ecf648749122f2b500e
hitmanDD <hitman_tt@mail.ru>
2022-10-29 19:52:44 +0300

    problem post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 5, minimum:10 }
+  validates :body, presence: true, length: { maximum: 10, minimum:1000 }
 end
---
+++ app/views/posts/#osnova.rb
@@ -0,0 +1,4 @@
+#osnova
+File.open("C:\Projects\sample.txt", "w") do |file|
+  file.write("1")
+end 
