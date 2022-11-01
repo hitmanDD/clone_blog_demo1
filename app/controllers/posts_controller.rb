7894b2eeb752b670a39847487dbba209a12b461d
hitmanDD <hitman_tt@mail.ru>
2022-11-02 00:44:33 +0300

    comet

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,8 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  # GET /posts/1 or /posts/1.json
-  def show
-  end
+  # GET /posts/1 or /posts/1.json izmenil :end
+  def show; end
 
   # GET /posts/new
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 5, minimum:10 }
-  validates :body, presence: true, length: { maximum: 10, minimum:1000 }
+  validates :title, presence: true, length: { maximum: 100, minimum:5 }
+  validates :body, presence: true, length: { maximum: 10000, minimum:10 }
 end
