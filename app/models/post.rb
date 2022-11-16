7ea80b23061922fd87e467467449f62bad89d1da
hitmanDD <hitman_tt@mail.ru>
2022-11-16 23:02:04 +0300

    Edit Account

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100, minimum:5 }
   validates :body, presence: true, length: { maximum: 10000, minimum:10 }
+  belongs_to :user
 end
